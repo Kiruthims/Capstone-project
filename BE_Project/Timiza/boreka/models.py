91346e002a2fcbb0a4b2555ddfc99fc8f9c978f1
Kiruthims <Kiruthimiss@proton.me>
2025-03-29 23:58:21 +0300

    Added & defined status and priority choices in my task model

--- BE_Project/Timiza/boreka/models.py
+++ BE_Project/Timiza/boreka/models.py
@@ -1,14 +1,26 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  
 from django.db import models
 
-
-
 class Task(models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE, related_name ="tasks")
+    STATUS_CHOICES = [
+        ("Pending", "Pending"),
+        ("Completed", "Completed"),
+    ]
+
+
+    PRIORITY_CHOICES = [
+        ("Low", "Low"),
+        ("Medium", "Medium"),
+        ("High", "High"),
+    ]
+    
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
     title = models.CharField(max_length=200)
-    description = models.TextField(blank = True, null = True)
-    is_completed = models.BooleanField(default = False)
-    due_date = models.DateField(blank = True, null = True)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="Pending")  
+    due_date = models.DateField(blank=True, null=True)
+    priority_level = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default="Medium")
 
     def __str__(self):
         return self.title
