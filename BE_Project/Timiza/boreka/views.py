6eaa02341119e3ff3b63e07c55650f14e4995852
Kiruthims <Kiruthimiss@proton.me>
2025-03-30 22:46:33 +0300

    Authentication done: login, logout and isauthenticated on user

--- BE_Project/Timiza/boreka/views.py
+++ BE_Project/Timiza/boreka/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.permissions import IsAuthenticated
 
+
+
+
+class RegisterUserAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response({"error": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+
+
+class LoginUserAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutUserAPIView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"message": "Logout successful"}, status=status.HTTP_200_OK)
+
+
+
+ #class ListCreateAPIView(APIView):
+#    pass
+
+
+#class RetrieveUpdateDeleteAPIView(APIView):
+#    pass
+
+
+#def __str__(self):
+#    return self.
 # Create your views here.
