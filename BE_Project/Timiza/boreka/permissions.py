62e068e440ab302ac7eec782b65751fbcd387d52
Kiruthims <Kiruthimiss@proton.me>
2025-04-04 16:51:57 +0300

    Updated permissions to block hugo from modifying tasks

--- BE_Project/Timiza/boreka/permissions.py
+++ BE_Project/Timiza/boreka/permissions.py
@@ -6,8 +6,7 @@ class IsAdminUser(BasePermission):
         return request.user and request.user.is_staff
 
 class IsTaskOwner(BasePermission):
-    """
-    Custom permission to allow only task owners to edit or delete their own tasks.
-    """
     def has_object_permission(self, request, view, obj):
-        return request.user. is_staff or obj.user == request.user
+        if request.method in ["PUT", "PATCH", "DELETE"]:  # Hugo and allow others since their perm allow
+            return False
+        return obj.user == request.user  
