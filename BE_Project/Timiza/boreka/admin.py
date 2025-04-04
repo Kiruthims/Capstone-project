1552cc23b46e8f03c9db4e7e58ff4adada4ef60e
Kiruthims <Kiruthimiss@proton.me>
2025-04-04 11:30:06 +0300

    Override the get_queryset so that users can only view their own tasks in admin panel

--- BE_Project/Timiza/boreka/admin.py
+++ BE_Project/Timiza/boreka/admin.py
@@ -5,7 +5,15 @@ from .models import Task
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'user', 'due_date', 'status']
     search_fields = ['title', 'user__username']
-    list_filter = ['status', 'due_date']
+    list_filter = ['status', 'due_date', 'user__username']
+
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        # If not a superuser, filter to show only their own tasks
+        if not request.user.is_superuser:
+            queryset = queryset.filter(user=request.user)
+        return queryset
 
 
 admin.site.register(Task, TaskAdmin)
