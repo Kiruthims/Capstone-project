68be3d92c9d1f54299cb4dcd17636d7050a511cf
Kiruthims <Kiruthimiss@proton.me>
2025-04-06 06:10:05 +0300

    Configure database connection for Render deployment (using external URL)

--- BE_Project/Timiza/Timiza/settings.py
+++ BE_Project/Timiza/Timiza/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -93,17 +97,12 @@ WSGI_APPLICATION = 'Timiza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_database_name',  
-        'USER': 'your_database_user',  
-        'PASSWORD': 'your_database_password',  
-        'HOST': 'your_database_host',  
-        'PORT': '5432',  
-}
-}
 
+
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgresql://capstone_db_es79_user:3j8jH2XbkzVmZAPvbOUrSCpOoxlnZsGV@dpg-cvolgahr0fns739pu5bg-a.oregon-postgres.render.com/capstone_db_es79')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -140,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
